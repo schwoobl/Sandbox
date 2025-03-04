5b069fcb1ce686906fe791422346a0459f42aeaf
schwoobl <mike_holz@gmx.net>
2025-03-04 21:43:07 +0100

    Update project.py

--- project.py
+++ project.py
@@ -61,3 +61,32 @@ else:
 fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print(fruit)
+
+#Print example with variable
+hi = ("Hello, World!")
+print(hi)
+
+#String exmaple
+name = "Mike"
+#Integer example
+age = 34
+#Float example
+height = 1.80
+#Boolean Example
+is_student = False
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student?", is_student)
+
+#List example
+fruits = ["apple", "banana", "cherry"]
+print("List example:", fruits)
+
+#Tuple example
+coordinates = (3, 7)
+print("Tuple example:", coordinates)
+
+#Dictionary example
+person = {"name" : "Alice", "age" : 25, "is_student" : True}
+print("Dictionary example:", person)
