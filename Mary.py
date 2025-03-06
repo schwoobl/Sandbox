ee70f00f09fdb2dc1edfd41dfb6dfe8d0aa58024
schwoobl <mike_holz@gmx.net>
2025-03-06 17:11:09 +0100

    Add files via upload

---
+++ Mary.py
@@ -0,0 +1,11 @@
+name = "Mary"
+password = "swordfish"
+
+if name == "Mary":
+    print("Hello, " + name)
+    
+    if password == "swordfish":
+        print("Access granted.")
+        
+    else:
+        print("Access denied.")
