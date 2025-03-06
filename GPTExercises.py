f95e4638750b792754ff17e3922e6f293a56ae43
schwoobl <mike_holz@gmx.net>
2025-03-06 10:07:29 +0100

    Add files via upload

---
+++ GPTExercises.py
@@ -0,0 +1,17 @@
+#print("Enter the first number:")
+#NumberOne = input()
+#print("Enter the second number: ")
+#NumberTwo = input()
+
+#print("Sum: ", int(NumberOne) + int(NumberTwo))
+#print("Difference: ", int(NumberOne) - int(NumberTwo))
+#print("Product: ", int(NumberOne) * int(NumberTwo))
+#print("Quotient: ", int(NumberOne) // int(NumberTwo))
+
+print("Enter a number:")
+Number = input()
+if int(Number) % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+    
