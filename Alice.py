728dcd48166471859eb82291ee35d705c5ce7037
schwoobl <mike_holz@gmx.net>
2025-03-06 17:22:02 +0100

    Add files via upload

---
+++ Alice.py
@@ -0,0 +1,9 @@
+age = 13
+print("What's your name?")
+name = input()
+if name == "Alice":
+    print("Hi, Alice!")
+elif age < 12:
+    print("You are not Alice, kiddo!")
+else:
+    print("You are neither Alice nor a little kid.")
