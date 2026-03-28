c7c918d605ca0d170ea0111fc1b87b47e6e89f44
schwoobl <mike_holz@gmx.net>
2026-03-28 11:56:29 +0000

    Update

--- Practice Files/collatzSequence.py
+++ Practice Files/collatzSequence.py
@@ -1,3 +1,32 @@
-def collatz(number):
-    if (number // 2) == 0:
+# def collatz(number):
+#     if number % 2 == 0:
+#         number = number // 2
+#         print(number)
+#     else:
+#         number = 3 * number + 1
+#         print(number)
+#     return number
 
+# user_input = int(input("Please enter a number:"))
+
+# while True:
+#     collatz(user_input)
+#     print(collatz(user_input), sep=", ")
+#     return collatz()
+
+def collatz(local_number):
+    if local_number % 2 == 0:
+        result = local_number // 2
+    else:
+        result = 3 * local_number + 1
+    
+    print(result, end=' ')  # prints on one line
+    return result
+
+
+global_number = int(input("Enter a number: "))
+
+print(global_number, end=' ')  # print starting number
+
+while global_number != 1:
+    global_number = collatz(global_number)
--- Textgame/rockpaperscissors.py
+++ Textgame/rockpaperscissors.py
@@ -1,11 +1,20 @@
 import random
- #game logic:
+import sys
 
+ #game logic:
  #r > s, p > r, s > p
+
 def main():
 
-    player_choice = input("(R)ock, (p)aper or (s)cissors?!").strip().lower()
-    computer_choice = random.choice(["r", "p", "s"])
-
-    if player_choice == computer_choice:
-        print ("It's a tie!")
+    while True: #Main game loop
+        player_choice = input("(R)ock, (p)aper or (s)cissors?! >").strip().lower()
+        computer_choice = random.choice(["r", "p", "s"])
+        while player_choice != "r" or "p" or "s" or "q":
+            print("Please enter valid input")
+        if player_choice == computer_choice:
+            print ("It's a tie!")
+        elif player_choice == "r" and computer_choice == "s" or player_choice == "s" and computer_choice == "p" or player_choice == "p" and computer_choice == "r":
+            print("Player wins!")
+        else:
+            print("Computer wins!")
+main()
