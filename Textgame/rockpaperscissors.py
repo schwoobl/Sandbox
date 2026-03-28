cf411cbc85ecdc9ed2f9f17c0cf6eeea78139c77
schwoobl <mike_holz@gmx.net>
2026-03-28 19:06:04 +0000

    update

--- Textgame/rockpaperscissors.py
+++ Textgame/rockpaperscissors.py
@@ -5,16 +5,29 @@ import sys
  #r > s, p > r, s > p
 
 def main():
+    wins = 0
+    losses = 0
+    ties = 0
 
     while True: #Main game loop
-        player_choice = input("(R)ock, (p)aper or (s)cissors?! >").strip().lower()
+        while True:
+            print(f"{wins} wins, {losses} losses, {ties} ties!")
+            player_choice = input("(R)ock, (p)aper or (s)cissors?! >").strip().lower()
+            if player_choice not in ["r", "p", "s", "q"]:
+                print("Please enter valid (r, p, s, q)!")
+            elif player_choice == "q":
+                sys.exit("Exiting..")
+            else:
+                break
         computer_choice = random.choice(["r", "p", "s"])
-        while player_choice != "r" or "p" or "s" or "q":
-            print("Please enter valid input")
+
         if player_choice == computer_choice:
             print ("It's a tie!")
+            ties += 1
         elif player_choice == "r" and computer_choice == "s" or player_choice == "s" and computer_choice == "p" or player_choice == "p" and computer_choice == "r":
             print("Player wins!")
+            wins += 1
         else:
             print("Computer wins!")
+            losses += 1
 main()
--- rockPaperScissorsNotMine.py
+++ rockPaperScissorsNotMine.py
@@ -12,7 +12,7 @@ while True: # The main game loop.
     print('%s Wins, %s losses, %s ties' % (wins, losses, ties))
     while True: # The player input loop.
         print('Enter your move: (r)ock, (p)aper, (s)cissors or (q)uit')
-        playerMove = input()
+        playerMove = input(">")
         if playerMove == 'q':
             sys.exit() # Quit the program.
         if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
