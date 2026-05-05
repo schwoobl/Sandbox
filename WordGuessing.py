755adc3533e87387315014effbd865c0fa250786
schwoobl <mike_holz@gmx.net>
2026-05-05 17:37:14 +0000

    Update

--- WordGuessing.py
+++ WordGuessing.py
@@ -2,11 +2,17 @@ import random
 
 #Guess the word game
 
-words = [ "Hello" , "Test" , "Help"]
-
+words = [ "Hello" , "Test" , "Help", "pasta", "balloon"]
 word = random.choice(words)
+guesses = 10
 
-print("Guess the characters")
 
-for chr in word:
-    print(chr)
+while guesses != 0:
+    guess = input("Guess the word >")
+    
+    for letter in word:
+        if guess not in word:
+            print("Nope, wrong. Guess again.")
+            guesses -= 1
+        elif guess in word:
+            print(letter)
