9c8caa042eb852b2852ccf3127c09c48e94b7b25
schwoobl <mike_holz@gmx.net>
2026-08-16 19:03:17 +0000

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   Practice Files/boxPrint.py
    	new file:   Practice Files/factorialLog.py
    	modified:   Practice Files/forLoop.py

--- Practice Files/forLoop.py
+++ Practice Files/forLoop.py
@@ -1,8 +1,9 @@
 print("Hello!")
+
 for i in range(5):
     print("On this iteration, i is set to: " + str(i))
 
 import random
 
 for i in range(5):
-    print(random.randint(1,10))
+    print(random.randint(1,10), end= ' ')
